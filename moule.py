864602f971a19ae81cc248d6cfe374e30c98a137
Xlebynya <Xlebynya@gmail.com>
2025-03-14 00:28:14 +0300

    (feat) parsed wishlist

--- moule.py
+++ moule.py
@@ -1,6 +1,5 @@
 import lxml.html
 import requests
-import lxml
 import fake_useragent
 
 session = requests.Session()
@@ -20,6 +19,49 @@ response = session.post(URL, data=payload, headers=header)
 html_wishlist = session.get("https://siriust.ru/wishlist/", headers=header).text
 tree = lxml.html.document_fromstring(html_wishlist)
 
+product_elements = tree.xpath('//div[@class="col-tile"]')
 
-with open("site.html", "w", encoding="utf-8") as f:
-    f.write(html_wishlist)
+products = []
+for element in product_elements:
+    try:
+        name = element.xpath('.//a[@class="product-title"]/text()')
+        price_roz, price_opt = [
+            float(el.replace("\xa0", ""))
+            for el in element.xpath('.//span[@class="ty-price-num"]/text()')
+        ]
+
+        link = element.xpath('.//span[@class="ty-nowrap ty-stars"]//@href')
+        product_html = session.get(link[0], headers=header).text
+        product_page = lxml.html.document_fromstring(product_html)
+        rating = product_page.xpath(
+            './/div[@itemprop="aggregateRating"]/meta[@itemprop="ratingValue"]/@content'
+        )
+        reviewCount = product_page.xpath(
+            './/div[@itemprop="aggregateRating"]/meta[@itemprop="reviewCount"]/@content'
+        )
+
+        store_count = int(
+            product_page.xpath('count(//*[@id="content_features"]/div/div)')
+        )
+
+        reviews = product_page.xpath(
+            '//div[@id="content_discussion_block"]//div[@class="ty-discussion-post__message"]/text()'
+        )
+        reviews = [it.replace("\r\n", "") for it in reviews]
+
+        product_data = {
+            "name": name,
+            "price_roz": price_roz,
+            "price_opt": price_opt,
+            "rating": rating,
+            "review_count": reviewCount,
+            "store_count": store_count,
+            "reviews": reviews,
+        }
+        products.append(product_data)
+
+    except Exception as e:
+        print(e)  # Отладочное сообщение
+
+for prod in products:
+    print(prod)
